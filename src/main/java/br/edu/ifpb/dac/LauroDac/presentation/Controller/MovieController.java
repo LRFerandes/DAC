fcf0bca62898783f3d4cc63641126905714b2785
Lauro Nunes <104803481+LRFerandes@users.noreply.github.com>
2023-08-25 11:04:18 -0300

    API REST
    adicionando anotações para utilização da API REST

--- src/main/java/br/edu/ifpb/dac/LauroDac/presentation/Controller/MovieController.java
+++ src/main/java/br/edu/ifpb/dac/LauroDac/presentation/Controller/MovieController.java
@@ -4,35 +4,47 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifpb.dac.LauroDac.bussines.service.MovieService;
 import br.edu.ifpb.dac.LauroDac.model.entity.Movie;
 import br.edu.ifpb.dac.LauroDac.model.entity.Session;
 import br.edu.ifpb.dac.LauroDac.model.repositories.MovieRepository;
 
-@Controller
+@RestController
 public class MovieController {
 	
 	@Autowired
 	private MovieService filmeService;
 	
-	public void saveMovie(Movie filme) {
+	@PostMapping("/Movie")
+	public void saveMovie(@RequestBody Movie filme) {
 		filmeService.Create(filme);
 	}
 	
-	public void ChangeMovieName(Integer id, String Nome) {
+	@PutMapping("/Movie/{id}")
+	public void ChangeMovieName(@PathVariable Integer id, @RequestBody String Nome) {
 		filmeService.UpdateNome(id, Nome);
 	}
 	
-	public void DeleteMovie(Integer id) {
+	@DeleteMapping("/Movie/{id}")
+	public void DeleteMovie(@PathVariable("id") Integer id) {
 		filmeService.Delete(id);
 	}
 	
+	@GetMapping("/Movie")
 	public List<Movie> ListMovies(){
 		return filmeService.Read();
 	}
 	
-	public void AddSession(Integer id, Session sessao) {
+	@PutMapping("/Movie/{id}")
+	public void AddSession(@PathVariable Integer id, @RequestBody Session sessao) {
 		filmeService.AddSession(id, sessao);
 	}
 	
--- src/main/java/br/edu/ifpb/dac/LauroDac/presentation/Controller/SessionController.java
+++ src/main/java/br/edu/ifpb/dac/LauroDac/presentation/Controller/SessionController.java
@@ -5,32 +5,46 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifpb.dac.LauroDac.bussines.service.MovieService;
 import br.edu.ifpb.dac.LauroDac.bussines.service.SessionService;
 import br.edu.ifpb.dac.LauroDac.model.entity.Movie;
 import br.edu.ifpb.dac.LauroDac.model.entity.Session;
 
-@Controller
+@RestController
 public class SessionController {
 	
 	@Autowired
 	private SessionService sessaoService;
 	
-	public void saveSession(Session sessao) {
+	
+	@PostMapping("/Session")
+	public void saveSession(@RequestBody Session sessao) {
 		sessaoService.Create(sessao);
 	}
 	
+	@GetMapping("/Session")
 	public List<Session> ListSession() {
 		return sessaoService.Read();
 	}
 	
-	public void DeleteSession(Integer id) {
+	@DeleteMapping("/Session/{id}")
+	public void DeleteSession(@PathVariable("id") Integer id) {
 		sessaoService.Delete(id);
 	}
 	
-	public void changeMovieOnPoster(Integer id, String fime) {
+	@PutMapping("/Session/{id}")
+	public void changeMovieOnPoster(@PathVariable Integer id, @RequestBody String fime) {
 		sessaoService.UpdateFilme(id, fime);
 	}
+	
+	
 
 }
